fbb5a3399e4fa184bfe7e8d543a2b3e690832571
gerson <gersonmoliver@gmail.com>
2024-09-27 00:14:42 -0700

    C# Prep 1—Variables, Input, and Output

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,27 @@
+/*
+ * File: Program.cs
+ * Author: Gerson Martins de Oliveira
+ * Date: September 21, 2024
+  * 
+ */
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Console.WriteLine("Hello Prep1 World!");
+
+        Console.Clear();
+
+        Console.WriteLine("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.WriteLine("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {firstName}, {firstName} {lastName}.");
+
     }
 }
